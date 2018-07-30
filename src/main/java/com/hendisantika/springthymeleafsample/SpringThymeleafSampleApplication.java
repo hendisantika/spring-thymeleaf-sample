0bccacea501783d810df54cbff53ccb5142cf6ee
Hendi Santika <hendisantika@gmail.com>
2018-07-31 06:50:59 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/springthymeleafsample/SpringThymeleafSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springthymeleafsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafSampleApplication.class, args);
+    }
+}
